3e8391bd999c14d83fa61809b12e41a1f6d57615
paroteen <104207441+paroteen@users.noreply.github.com>
2023-03-27 17:11:46 +0200

    100 atoic

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,26 +1,43 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * _atoi - converts strings to int
- * @s: first parameter
- *
- * Description: takes all types of char
- * Return: Always(0) Success
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: integers in a string
  */
+
 int _atoi(char *s)
 {
-int result = 0;
-int sign = 1;
-int i = 0;
+	int num, len, numcount, mult, x, neg;
 
-if (s[0] == '-')
-{
-sign = -1;
-i++;
-}
-for (; s[i] != '\0'; ++i)
-result = result * 10 + s[i] - '0';
-return (sign *result);
+	len = 0;
+	x = 0;
+	numcount = 0;
+	mult = 1;
+	neg = 1;
+	num = 0;
+
+	while (s[len] != '\0')
+	{
+		if (s[len] >= '0' && s[len] <= '9')
+		{
+			numcount++;
+			if (!(s[len + 1] >= '0' && s[len + 1] <= '9'))
+				break;
+		}
+		len++;
+	}
+	for (; numcount > 1; numcount--)
+		mult *= 10;
+	for (; x <= len; x++)
+	{
+		if (s[x] == '-')
+			neg *= -1;
+		else if (s[x] <= '9' && s[x] >= '0')
+		{
+			num += (s[x] - '0') * mult * neg;
+			mult /= 10;
+		}
+	}
+	return (num);
 }
